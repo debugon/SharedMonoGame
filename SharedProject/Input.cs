9846f78b54b61b8852cdcd74f985080268f63d02
debugon <games0421@gmail.com>
2018-09-04 18:30:24 +0900

    入力用のクラスInputを作成

---
+++ SharedProject/Input.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace SharedProject
+{
+    class Input
+    {
+        private KeyboardState oldKeyState;
+        private KeyboardState newKeyState;
+
+        public void Init()
+        {
+            Update();
+        }
+
+        public void Update()
+        {
+            oldKeyState = newKeyState;
+            newKeyState = Keyboard.GetState();
+        }
+
+        public bool IsJustKeyDown(Keys key)
+        {
+            bool down = false;
+
+            if(oldKeyState.IsKeyUp(key) && newKeyState.IsKeyDown(key))
+            {
+                down = true;
+            }
+
+            return down;
+        }
+
+        public bool IsJustKeyUp(Keys key)
+        {
+            bool up = false;
+
+            if (oldKeyState.IsKeyDown(key) && newKeyState.IsKeyUp(key))
+            {
+                up = true;
+            }
+
+            return up;
+        }
+
+        public bool IsKeyDown(Keys key)
+        {
+            return Keyboard.GetState().IsKeyDown(key);
+        }
+
+        public bool IsKeyUp(Keys key)
+        {
+            return Keyboard.GetState().IsKeyUp(key);
+        }
+    }
+}
